8917260dc4f482e9c09d4a6bc8fae9ee958382ca
Rahul Aggarwal <rahaggar@adobe.com>
2016-03-06 19:56:28 +0530

    3. path for build change.

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @Echo off
-cd C:\Users\rahaggar\DevTools\TrainingProject\myproject
+cd C:\Users\rahaggar\DevTools\TrainingProject\mytrainingproject
 cmd /C mvn clean install -Dmaven.test.skip=true -PautoInstallPackage
