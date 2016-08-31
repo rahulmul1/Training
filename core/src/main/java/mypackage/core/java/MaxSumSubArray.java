4f272392249b040ec7927adac24888ae9b7853e5
Rahul Aggarwal <rahaggar@adobe.com>
2016-08-31 15:44:29 +0530

    CARE-22: commiting java interview questions

--- core/src/main/java/mypackage/core/java/MaxSumSubArray.java
+++ core/src/main/java/mypackage/core/java/MaxSumSubArray.java
@@ -1,21 +1,24 @@
 package mypackage.core.java;
 
 public class MaxSumSubArray {
-	
+
 	public static void main(String[] args) {
+
+		int a[] = { -4, -1, 2, -2, 6, -3 };
 		
-		int a[] = {4, -1, 2, -2, -1, -3};
-		
-		int maxsum=0;
-		int currentmax=0;
-		
+		int maxSum = 0;
+		int currentmaxsum = 0;
+
 		for (int i = 0; i < a.length; i++) {
-			
-			
-			
+
+			currentmaxsum += a[i];
+			if (currentmaxsum > maxSum) {
+				maxSum = currentmaxsum;
+			}
+			if (currentmaxsum < 0) {
+				currentmaxsum = 0;
+			}
 		}
-		
-		
+		System.out.println(maxSum);
 	}
-	
 }
